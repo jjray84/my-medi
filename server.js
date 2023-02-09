4a5fdd623aa3961ccd41294385a664e649ebed26
Jonathan Ray <jonathanjudsonray@gmail.com>
2023-02-09 14:08:01 -0500

    Added path and comments to server.js file

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+// Dependencies
 const express = require('express');
+const path = require('path');
+
+// Sets up the express app
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('public'));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Starts the server and will display message in Integrated Terminal
 app.listen(PORT, () => {
-    console.log('Now listening at http://localhost: ${PORT}');
+    console.log('Now listening');
 });
