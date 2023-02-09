2cfc754c7008540748e8e7e6e1dbf37799cf2d49
kaguilera23 <118678899+kaguilera23@users.noreply.github.com>
2023-02-09 13:27:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
-    console.log('Now listening at http://locaslhost: ${PORT}');
+    console.log('Now listening at http://localhost: ${PORT}');
 });
