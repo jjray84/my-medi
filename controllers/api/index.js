42e4935287771594bf3fd4a4bddc28d51ed662b2
devasmi <impokhrelasmita5@gmail.com>
2023-02-10 12:46:57 -0500

    added logic to logout

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const signUpRoute = require("./signup");
-const loginRoute = require("./login")
+const loginRoute = require("./login");
+const logoutRoute = require("./logout")
 
 router.use("/signup", signUpRoute);
-router.use("/login", loginRoute)
+router.use("/login", loginRoute);
+router.use("/logout",logoutRoute);
 
 module.exports = router;
