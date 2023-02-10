f47570f509102929b5d12787e2057b348715c0cd
unknown <joseph.harley02@gmail.com>
2023-02-10 12:55:52 -0500

    added post request for /api/medications to create a new medication, and fixed a syntax error in medication model

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const signUpRoute = require("./signup");
-const loginRoute = require("./login")
+const loginRoute = require("./login");
+const medicationRoutes = require("./medicationRoutes");
 
 router.use("/signup", signUpRoute);
-router.use("/login", loginRoute)
+router.use("/login", loginRoute);
+router.use("/medication", medicationRoutes);
 
 module.exports = router;
