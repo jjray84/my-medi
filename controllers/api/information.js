cba1b3520adcf149842188409f5dc21353b771b0
unknown <aguilera.k02@gmail.com>
2023-02-14 12:15:54 -0500

    add and edit information

--- controllers/api/information.js
+++ controllers/api/information.js
@@ -28,16 +28,20 @@ router.get("/", previousUser, async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     const newInformation = await Information.create({
-      // personal_number: req.body.personal_number,
-      // emergency_contact: req.body.emergency_contact,
-      // emergency_number: req.body.emergency_number,
-      // blood_type: req.body.blood_type,
-      // allergies: req.body.allergies,
-      // transplants: req.body.transplants,
-      // devices: req.body.devices,
-      // user_id: req.body.user_id,
       ...req.body,
     });
+    res.json(newInformation);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
+});
+
+router.put("/", async (req, res) => {
+  try {
+    const newInformation = await Information.update({
+      ...req.body,
+    },{where: {user_id: req.session.user_id, }});
     res.json(newInformation);
   } catch (err) {
     console.error(err);
